746cd3e5ca9566a0461de0a648551e0d772de0a4
David Chambers <dc@davidchambers.me>
2020-09-02 14:59:26 +0200

    js

--- index2.js
+++ index2.js
@@ -9,6 +9,10 @@ const Z = require ('sanctuary-type-classes');
 
 const $ = {};
 
+const I = x => x;
+
+const K = x => y => x;
+
 const log = (...args) => {
   console.log (...args);
 };
@@ -32,7 +36,75 @@ const showEnv = env => {
   return `[${(String (env._ts)).padStart (2, '0')}]${'a' in env ? ` a = ${env['a']}` : ''}${'b' in env ? ` b = ${env['b']}` : ''}`;
 };
 
+$.Unknown = {
+  type: 'UNKNOWN',
+  name: '',
+  url: '',
+  supertypes: [],
+  arity: 0,
+  types: {},
+  _test: K (K (true)),
+  format: (outer, inner) => 'Unknown',
+};
+
+$.NullaryType = name => url => supertypes => test => ({
+  type: 'NULLARY',
+  name: name,
+  url: url,
+  supertypes: supertypes,
+  arity: 0,
+  types: {},
+  _test: K (test),
+  format: (outer, inner) => outer (name),
+});
+
+$.UnaryType = name => url => supertypes => test => _1 => $1 => ({
+  type: 'UNARY',
+  name: name,
+  url: url,
+  supertypes: supertypes,
+  arity: 1,
+  types: {$1: $1},
+  _test: K (test),
+  format: (outer, inner) => (
+    outer (name) +
+    outer (' ') +
+    when ($1.arity > 0)
+         (parenthesize (outer))
+         (inner ('$1') (show ($1)))
+  ),
+});
+
+$.BinaryType = name => url => supertypes => test => _1 => _2 => $1 => $2 => ({
+  type: 'BINARY',
+  name: name,
+  url: url,
+  supertypes: supertypes,
+  arity: 2,
+  types: {$1: $1, $2: $2},
+  _test: K (test),
+  format: (outer, inner) => (
+    outer (name) +
+    outer (' ') +
+    when ($1.arity > 0)
+         (parenthesize (outer))
+         (inner ('$1') (show ($1))) +
+    outer (' ') +
+    when ($2.arity > 0)
+         (parenthesize (outer))
+         (inner ('$2') (show ($2)))
+  ),
+});
+
 $.TypeVariable = name => ({
+  type: 'VARIABLE',
+  name: name,
+  url: '',
+  supertypes: [],
+  arity: 0,
+  types: {},
+  _test: K (K (true)),
+  format: (outer, inner) => name,
   new: env => x => {
     const these = types_ (x);
     if (name in env) {
@@ -46,45 +118,116 @@ $.TypeVariable = name => ({
   },
 });
 
+$.UnaryTypeVariable = name => $1 => ({
+  type: 'VARIABLE',
+  name: name,
+  url: '',
+  supertypes: [],
+  arity: 1,
+  types: {$1: $1},
+  _test: K (K (true)),
+  format: (outer, inner) => (
+    outer (name) +
+    outer (' ') +
+    when ($1.arity > 0)
+         (parenthesize (outer))
+         (inner ('$1') (show ($1)))
+  ),
+});
+
+$.BinaryTypeVariable = name => $1 => $2 => ({
+  type: 'VARIABLE',
+  name: name,
+  url: '',
+  supertypes: [],
+  arity: 2,
+  types: {$1: $1, $2: $2},
+  _test: K (K (true)),
+  format: (outer, inner) => (
+    outer (name) +
+    outer (' ') +
+    when ($1.arity > 0)
+         (parenthesize (outer))
+         (inner ('$1') (show ($1))) +
+    outer (' ') +
+    when ($2.arity > 0)
+         (parenthesize (outer))
+         (inner ('$2') (show ($2)))
+  ),
+});
+
+$.Function = types => ({
+  type: 'FUNCTION',
+  name: '',
+  url: '',
+  supertypes: [],
+  arity: types.length,
+  types: {$1: types[0], $2: types[1]},
+  _test: K (K (true)),
+  format: (outer, inner) => (
+    when (types.length !== 2)
+         (parenthesize (outer))
+         ('TK') +
+    outer (' -> ') +
+    inner ('TK')
+  ),
+});
+
 const a = $.TypeVariable ('a');
 const b = $.TypeVariable ('b');
 
-$.Number = {
-  new: env => x => {
-    if (typeof x !== 'number') throw new TypeError (`Not a number: ${JSON.stringify (x)}`);
-    return x;
-  },
-};
+$.Number = Object.assign (
+  $.NullaryType ('Number') ('url') ([]) (x => typeof x === 'number'),
+  {
+    new: env => x => {
+      if (typeof x !== 'number') throw new TypeError (`Not a number: ${JSON.stringify (x)}`);
+      return x;
+    },
+  }
+);
 
-$.String = {
-  new: env => x => {
-    if (typeof x !== 'string') throw new TypeError (`Not a string: ${JSON.stringify (x)}`);
-    return x;
-  },
-};
+$.String = Object.assign (
+  $.NullaryType ('String') ('url') ([]) (x => typeof x === 'string'),
+  {
+    new: env => x => {
+      if (typeof x !== 'string') throw new TypeError (`Not a string: ${JSON.stringify (x)}`);
+      return x;
+    },
+  }
+);
 
-$.Array = $1 => ({
-  new: env => x => {
-    if (!(Array.isArray (x))) throw new TypeError ('Not an array');
+$.Array = $1 => (
+  Object.assign (
+    $.UnaryType ('Array') ('url') ([]) (Array.isArray) (I) ($1),
+    {
+      new: env => x => {
+        if (!(Array.isArray (x))) throw new TypeError ('Not an array');
 
-    x.forEach (x => { $1.new (env) (x); });
+        x.forEach (x => { $1.new (env) (x); });
 
-    return x;
-  },
-});
+        return x;
+      },
+    }
+  )
+);
 
-$.Fn = $1 => $2 => ({
-  new: env => f => {
-    if (typeof f !== 'function') throw new TypeError ('Not a function');
-    return x => {
-      const i = $1.new (env) (x);
-      log ('updateEnv 3', showEnv (env));
-      const o = $2.new (env) (f (x));
-      log ('updateEnv 4', showEnv (env));
-      return o;
-    };
-  },
-});
+$.Fn = $1 => $2 => (
+  Object.assign (
+    $.Function ([$1, $2]),
+    {
+      new: env => f => {
+        if (typeof f !== 'function') throw new TypeError ('Not a function');
+        return x => {
+          const i = $1.new (env) (x);
+          log ('updateEnv 3', showEnv (env));
+          const o = $2.new (env) (f (x));
+          log ('updateEnv 4', showEnv (env));
+          return o;
+        };
+      },
+    }
+  )
+);
 
 const def = name => constraints => types => {
   const [output, ...inputs] = Z.reverse (types.map (t => t.new));
